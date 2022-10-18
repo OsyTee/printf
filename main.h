2c99366464a9434bbea7a8c32fd936ff334356bc
OsyTee <{mecus4life@gmail.com}>
2022-10-17 17:04:19 -0700

    commit

--- main.h
+++ main.h
@@ -1,101 +1,61 @@
-#ifndef _PRINTF_H
-#define _PRINTF_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdarg.h>
-
-#define FIELD_BUF_SIZE 50
-#define OUTPUT_BUF_SIZE 1024
-#define NULL_STRING "(null)"
-
-#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-#define BUF_FLUSH -1
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
- * struct parameters - parameters structs
- * @unsign: flag if unsigned value is true
- * @plus_flag: flag yes if plus_flag specified
- * @space_flag: flag if space_tag specified
- * @hashtag_flag: on if _flag specified
- * @zero_flag: on if _flag specified
- * @minus_flag: on if _flag specified
- * @width: field width specified
- * @precision: field precision specified
- * @h_modifier: on if h_modifier is specified
- * @l_modifier: on if l_modifier is specified
+ * struct print - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions.
  */
-typedef struct parameters
+typedef struct print
 {
-	unsigned int unsign		: 1;
-	unsigned int plus_flag		: 1;
-	unsigned int space_flag		: 1;
-	unsigned int hashtag_flag	: 1;
-	unsigned int zero_flag		: 1;
-	unsigned int minus_flag		: 1;
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
 
-	unsigned int width;
-	unsigned int precision;
-
-	unsigned int h_modifier		: 1;
-	unsigned int l_modifier		: 1;
-} params_t;
-
-/**
- * struct specifier def
- * @specifier: format token
- * @f: The function associated
- */
-typedef struct specifier
-{
-	char *specifier;
-	int (*f)(va_list, params_t *);
-} specifier_t;
-
-/* otherfunctions .c module */
-int print_int(va_list ap, params_t *params);                                                                                     
-int print_string(va_list ap, params_t *params);                                                                                  
-int print_percent(va_list ap, params_t *params);                                                                                 
-int print_char(va_list ap, params_t *params);                                                                                    
-int print_S(va_list ap, params_t *params); 
-
-/* _modules in putchar */
-int _putchar(int c);
-int _puts(char *str);
-
-/* number.c module */
-char *convert(long int num, int base, int flags, params_t *params);
-int print_unsigned(va_list ap, params_t *params);
-int print_address(va_list ap, params_t *params);
-
-/* conversion functions */
-int print_hex(va_list ap, params_t *params);
-int print_HEX(va_list ap, params_t *params);
-int print_binary(va_list ap, params_t *params);
-int print_octal(va_list ap, params_t *params);
-
-/* simple func */
-int print_from_to(char *start, char *stop, char *except);
-int print_rev(va_list ap, params_t *params);
-int print_rot13(va_list ap, params_t *params);         
-int (*get_specifier(char *s))(va_list ap, params_t *params);
-int get_print_func(char *s, va_list ap, params_t *params);
-void init_params(params_t *params, va_list ap);                                                                       
-char *get_precision(char *p, params_t *params, va_list ap);                                                                      
-int _printf(const char *format, ...); 
-int get_flag(char *s, params_t *params);                                                                                         
-int get_modifier(char *s, params_t *params);                                                                                     
-char *get_width(char *s, params_t *params, va_list ap);
-                                                      
-/* print_number.c module */
-int _isdigit(int c);
-int _strlen(char *s);
-int print_number(char *str, params_t *params);
-int print_number_right_shift(char *str, params_t *params);
-int print_number_left_shift(char *str, params_t *params);
+int _printf(const char *format, ...);
+int print_prg(va_list __attribute__((unused)), char *, unsigned int);
+int print_chr(va_list arguments, char *buf, unsigned int ibuf);
+int print_str(va_list arguments, char *buf, unsigned int ibuf);
+int print_int(va_list arguments, char *buf, unsigned int ibuf);
+int print_bnr(va_list arguments, char *buf, unsigned int ibuf);
+int print_unt(va_list arguments, char *buf, unsigned int ibuf);
+int print_oct(va_list arguments, char *buf, unsigned int ibuf);
+int print_hex(va_list arguments, char *buf, unsigned int ibuf);
+int print_upx(va_list arguments, char *buf, unsigned int ibuf);
+int print_usr(va_list arguments, char *buf, unsigned int ibuf);
+int print_add(va_list arguments, char *buf, unsigned int ibuf);
+int print_rev(va_list arguments, char *buf, unsigned int ibuf);
+int print_rot(va_list arguments, char *buf, unsigned int ibuf);
+int prinlint(va_list arguments, char *buf, unsigned int ibuf);
+int prinlunt(va_list arguments, char *buf, unsigned int ibuf);
+int prinloct(va_list arguments, char *buf, unsigned int ibuf);
+int prinlhex(va_list arguments, char *buf, unsigned int ibuf);
+int prinlupx(va_list arguments, char *buf, unsigned int ibuf);
+int prinhint(va_list arguments, char *buf, unsigned int ibuf);
+int prinhunt(va_list arguments, char *buf, unsigned int ibuf);
+int prinhoct(va_list arguments, char *buf, unsigned int ibuf);
+int prinhhex(va_list arguments, char *buf, unsigned int ibuf);
+int prinhupx(va_list arguments, char *buf, unsigned int ibuf);
+int prinpint(va_list arguments, char *buf, unsigned int ibuf);
+int prinnoct(va_list arguments, char *buf, unsigned int ibuf);
+int prinnhex(va_list arguments, char *buf, unsigned int ibuf);
+int prinnupx(va_list arguments, char *buf, unsigned int ibuf);
+int prinsint(va_list arguments, char *buf, unsigned int ibuf);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+int ev_print_func(const char *s, int index);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int print_buf(char *buf, unsigned int nbuf);
+char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
+char *fill_oct_array(char *bnr, char *oct);
+char *fill_long_oct_array(char *bnr, char *oct);
+char *fill_short_oct_array(char *bnr, char *oct);
+char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
 
 #endif
